426023ad17f39904b548fc6fb55153a859005382
Em <85346345+MgramTheDuck@users.noreply.github.com>
2025-06-22 20:55:24 +1000

    Update pingTool.ps1

--- pingTool.ps1
+++ pingTool.ps1
@@ -10,17 +10,17 @@
 
 .NOTES
     File Name      : PIngTool.ps1
-    Author         : MgramTheDuck
+    Author         : Em Gram
     Prerequisite   : PowerShell 5.1 or later
     Created        : June 22, 2025
-    Version        : 1.0
+    Version        : 1.1
 
 .EXAMPLE
     .\PIngTool.ps1
     Runs the script and prompts for an IP address or hostname to monitor
 
 .LINK
-    https://github.com/MgramTheDuck/PingTool
+    https://github.com/yourusername/PingTool
 #>
 
 $ip = Read-Host "Enter the IP address or hostname to ping"
@@ -55,10 +55,21 @@ while ($true) {
     $pingCount++
     $result = Test-Connection -ComputerName $ip -Count 1 -ErrorAction SilentlyContinue
     $timestamp = Get-Date -Format 'yyyy-MM-dd HH:mm:ss'
-    
-    if ($result) {
-        $time = $result.Latency
-        $status = $result.Status
+      if ($result) {
+        # Handle different property names in different PowerShell versions
+        if ($null -ne $result.Latency) {
+            # PowerShell 7+
+            $time = $result.Latency
+        }
+        elseif ($null -ne $result.ResponseTime) {
+            # PowerShell 5.1
+            $time = $result.ResponseTime
+        }
+        else {
+            # Even older versions might use this
+            $time = $result.ResponseTimeToLive
+        }
+        
         $address = $result.Address
         $bufferSize = $result.BufferSize
         
@@ -68,7 +79,7 @@ while ($true) {
         $maxTime = [Math]::Max($maxTime, $time)
         $avgTime = [Math]::Round($totalTime / $successCount, 2)
         
-        $msg = "$timestamp - Reply from $address : Latency=${time}ms, Status=$status, Buffer=$bufferSize bytes, Min=${minTime}ms, Max=${maxTime}ms, Avg=${avgTime}ms"
+        $msg = "$timestamp - Reply from $address : Latency=${time}ms, Buffer=$bufferSize bytes, Min=${minTime}ms, Max=${maxTime}ms, Avg=${avgTime}ms"
     } else {
         $failCount++
         $msg = "$timestamp - Request to $ip failed. Successful pings: $successCount/$pingCount"
